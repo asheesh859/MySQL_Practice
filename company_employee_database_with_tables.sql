133b1130287de91676350a0dba5cc736ffefd50e
asheesh859 <ak2305@checkboxtechnology.com>
2025-04-24 02:02:13 +0530

    Insert data into Employee Table

--- company_employee_database_with_tables.sql
+++ company_employee_database_with_tables.sql
@@ -42,4 +42,17 @@ alter table salaries
 	add foreign key (emp_no)
     references employees(emp_no);
     
-select * from salaries;
+insert into employees values(1, '2000-07-02', "Asheesh" , "Kumar" , 'M','2025-04-23'); 
+insert into employees values(2, '1999-01-03', "Akash" , "Sharma" , 'M','2025-05-17'); 
+insert into employees values(3, '2004-02-04', "Nimesh" , "Mishra" , 'M','2025-01-12'); 
+insert into employees values(4, '2003-03-05', "Saloni" , "Paliwal" , 'F','2025-08-6'); 
+insert into employees values(5, '2002-04-06', "Hanshika" , "Rathi" , 'F','2025-12-5'); 
+insert into employees values(6, '2001-05-07', "Priya" , "Bhagel" , 'F','2025-11-4'); 
+insert into employees values(7, '1998-06-08', "Manish" , "Kumar" , 'M','2025-09-3'); 
+insert into employees values(8, '1999-07-09', "Devesh" , "Kashyap" , 'M','2025-02-2'); 
+insert into employees values(9, '2002-08-10', "Aneesh" , "Sharma" , 'M','2025-03-1'); 
+insert into employees values(10, '2001-08-03', "Ankush" , "Kumar" , 'M','2025-03-21'); 
+
+
+
+
